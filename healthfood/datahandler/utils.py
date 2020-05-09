5cd2caa1472cf2c3956ad750eef801436f3dc89b
Nathan <skeks463@gmail.com>
2020-05-09 22:25:14 +0900

    Refactroing

--- healthfood/datahandler/utils.py
+++ healthfood/datahandler/utils.py
@@ -2,14 +2,21 @@ from MysqlHandler import MysqlHandler
 import pandas as pd
 
 
-def save_db_data(sql):
+def save_db_to_csv(sql: str, file_path: str) -> None:
+    """ db에 있는 데이터를 csv 파일로 변환.
+
+    :param sql: db sql 쿼리문
+    :param file_path: 저장할 파일 위치.
+    :return: None
+    """
     handler = MysqlHandler(host="localhost",
                                    user="root",
                                    password="",
                                    db="health_food_pjt",
                                    port=None)
     with handler:
-        handler.mysql_to_df("select * from register_info", save=True, file_path="./data.csv")
+        handler.mysql_to_df(sql, save=True, file_path=file_path)
+
 
 
 if __name__ == '__main__':
